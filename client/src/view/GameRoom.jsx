d54deb4295796217f132d1ab5ae1a3c640bb2b55
배윤호 <bem2183@naver.com>
2022-06-23 23:12:07 +0900

    feat: 게임페이지 틀 구현

--- client/src/view/GameRoom.jsx
+++ client/src/view/GameRoom.jsx
@@ -1,4 +1,9 @@
 import BackBtn from "../component/gameroom/BackBtn";
+import GameBoard from "../component/gameroom/GameBoard";
+import GameChatRoom from "../component/gameroom/GameChatRoom";
+import InformationBoard from "../component/gameroom/InformationBoard";
+import User1Info from "../component/gameroom/User1Info";
+import User2Info from "../component/gameroom/User2Info";
 
 const GameRoom = (props) => {
   const { setTap } = props;
@@ -6,6 +11,11 @@ const GameRoom = (props) => {
     <div>
       <p>게임방에 오신 것을 환영합니다.</p>
       <BackBtn setTap={setTap} />
+      <GameBoard />
+      <GameChatRoom />
+      <InformationBoard />
+      <User1Info />
+      <User2Info/>
     </div>
   );
 };
