6c2983b82a19f77a8a824be57ba177bd76dfe805
Ssoco <dlscjs8646@gmail.com>
2022-06-25 00:17:56 +0900

    feat: 방 생성 socket을 관리할 create함수 생성

---
+++ server/src/controller/socket/create.ts
@@ -0,0 +1,31 @@
+import type { RoomType, SocketPropType } from "@src/types";
+
+const createRoomCode = (rooms: RoomType) => {
+  while (true) {
+    const code = Math.random().toString(16).substr(2, 5);
+    if (!(code in rooms)) return code;
+  }
+};
+
+// create: 방 생성 socket 함수
+const create = ({
+  io,
+  socket,
+  rooms,
+  targetInfo,
+}: SocketPropType): SocketPropType => {
+  socket.on("CREATE_REQUEST", (user, roomName) => {
+    const roomCode = createRoomCode(rooms);
+    const sid = socket.id;
+    rooms[roomCode] = {
+      roomName,
+      hostSID: sid,
+      users: {},
+    };
+    rooms[roomCode].users[sid] = user;
+  });
+
+  return { io, socket, rooms, targetInfo };
+};
+
+export default create;
