dffae9092e706e9bf7cffc83024539b95350e06a
Ssoco <dlscjs8646@gmail.com>
2022-06-25 00:17:33 +0900

    feat: 상수로 사용할 요소들 별도로 분리

--- server/src/constant.ts
+++ server/src/constant.ts
@@ -1,1 +1,6 @@
 export const PORT = process.env.PORT || "5000";
+export const FRONT_BASE_URL = "http://localhost:3000";
+export const ERROR = {
+  NOT_EXIST_ROOM: "존재하지 않는 방입니다.",
+  UNAUTHORIZED_ROOM: "권한이 없습니다.",
+};
