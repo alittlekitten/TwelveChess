b3a7c6de692a4d95e58c8161fda2f06b77237afa
Ssoco <dlscjs8646@gmail.com>
2022-06-25 00:16:41 +0900

    feat: socket.io 연결을 위한 기본 세팅

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,9 +2,13 @@ import express, { Request, Response, NextFunction } from "express";
 import { PORT } from "@src/constant";
 import http from "http";
 import apiRouter from "@src/api";
+import Loader from "@src/loader";
 
 const app = express();
 const server = http.createServer(app);
+
+Loader({ server });
+
 app.set("port", PORT);
 app.use("/api", apiRouter);
 
