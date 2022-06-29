2c1787e82133e587d36bfc391672d1255afafb0a
Ssoco <dlscjs8646@gmail.com>
2022-06-30 03:20:30 +0900

    design: GameRoom.jsx의 디자인 일부 수정

--- client/src/component/gameroom/GameBoard.jsx
+++ client/src/component/gameroom/GameBoard.jsx
@@ -2,20 +2,20 @@
 import { css } from "@emotion/react";
 
 const GameBoard = () => {
-return (
+  return (
     <div css={GameBoardContainer}>
-    <p >게임판</p>
+      <p>게임판</p>
     </div>
-);
+  );
 };
 
 const GameBoardContainer = css`
-position: absolute;
-background-color: #b2b2b2;
-width: 30%;
-height: 60%;
-left: 30%;
-top: 20%;
+  position: absolute;
+  background-color: #b2b2b2;
+  width: 30%;
+  height: 60%;
+  left: 30%;
+  top: 20%;
 `;
 
 export default GameBoard;
