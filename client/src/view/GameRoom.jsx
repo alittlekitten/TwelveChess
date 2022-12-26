425c5bd9337f117bd962734887efa4fcdb82ccb0
Ssoco <dlscjs8646@gmail.com>
2022-12-26 17:08:23 +0900

    :wrench: 2차 환경설정 및 eslint, prettier 설정

--- client/src/view/GameRoom.jsx
+++ client/src/view/GameRoom.jsx
@@ -1,20 +1,20 @@
-import { useEffect } from "react";
-import BackBtn from "../component/gameroom/BackBtn";
-import GameBoard from "../component/gameroom/GameBoard";
-import GameChatRoom from "../component/gameroom/GameChatRoom";
-import InformationBoard from "../component/gameroom/InformationBoard";
-import User1Info from "../component/gameroom/User1Info";
-import User2Info from "../component/gameroom/User2Info";
-import Socket from "../socket/socket";
-import { roomsAPI } from "../api/room";
-import { useQuery } from "react-query";
-import { useSelector } from "react-redux";
+import { useEffect } from 'react';
+import BackBtn from '../component/gameroom/BackBtn';
+import GameBoard from '../component/gameroom/GameBoard';
+import GameChatRoom from '../component/gameroom/GameChatRoom';
+import InformationBoard from '../component/gameroom/InformationBoard';
+import User1Info from '../component/gameroom/User1Info';
+import User2Info from '../component/gameroom/User2Info';
+import Socket from '../socket/socket';
+import { roomsAPI } from '../api/room';
+import { useQuery } from 'react-query';
+import { useSelector } from 'react-redux';
 
 const GameRoom = () => {
   const roomCode = useSelector((state) => state.user.roomCode);
-  console.log("룸코드", roomCode);
-  const roomData = useQuery("roomData", () => roomsAPI(roomCode));
-  console.log("데이터1", roomData);
+  console.log('룸코드', roomCode);
+  const roomData = useQuery('roomData', () => roomsAPI(roomCode));
+  console.log('데이터1', roomData);
 
   useEffect(() => {
     Socket.connect();
