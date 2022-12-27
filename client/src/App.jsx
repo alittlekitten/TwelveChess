e44fbbec94b6e407880b8997fce60fdb9f047fb9
Ssoco <dlscjs8646@gmail.com>
2022-12-27 12:30:07 +0900

    :recycle: 라우팅 처리(미완)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
-import SetNickname from "./view/SetNickname";
-import { useState } from "react";
-import "./App.css";
-import WaitingRoom from "./view/WaitingRoom";
-import GameRoom from "./view/GameRoom";
-import { useSelector } from "react-redux";
+import SetNickname from './view/SetNickname';
+import { useState } from 'react';
+import './App.css';
+import WaitingRoom from './view/WaitingRoom';
+import GameRoom from './view/GameRoom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const tap = useSelector((state) => state.user.tap);
   return (
     <div className="App">
-      {tap === "SetNickname" && <SetNickname />}
-      {tap === "WaitingRoom" && <WaitingRoom />}
-      {tap === "GameRoom" && <GameRoom />}
+      {tap === 'SetNickname' && <SetNickname />}
+      {tap === 'WaitingRoom' && <WaitingRoom />}
+      {tap === 'GameRoom' && <GameRoom />}
     </div>
   );
 }
